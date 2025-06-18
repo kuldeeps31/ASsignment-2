68ebf5ca24a61e16458cd3e7722e6906934518c5
kuldeep <kuldeepsinghchouhan224@gmail.com>
2025-06-19 00:23:30 +0530

    react-setup

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <div className="app-container">
+      <h1>Note-Taking App</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
